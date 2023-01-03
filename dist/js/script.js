d70aff1f86be70c2bd5e8b14fceb9bc610d5924b
Yufri Luttu <yufridonluttu@gmail.com>
2023-01-03 14:46:17 +0800

    tambah halaman skills

--- dist/js/script.js
+++ dist/js/script.js
@@ -52,3 +52,16 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 } else {
   darkToggle.checked = false;
 }
+
+// load more button
+$(document).ready(function(){
+  $(".content").slice(0, 2).show();
+  $("#loadMore").on("click", function(e){
+    e.preventDefault();
+    $(".content:hidden").slice(0, 2).slideDown();
+    if($(".content:hidden").length == 0) {
+      $("#loadMore").text("No Content").addClass("noContent");
+    }
+  });
+  
+})
