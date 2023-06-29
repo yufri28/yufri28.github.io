ea28b1ec0659d102e231c841b557e95508129718
Yufri Luttu <yufridonluttu@gmail.com>
2023-06-29 14:09:26 +0800

    menambahkan kamus bahasa delha dan nav fix-top

--- kamus-bahasa-rote/kamus.js
+++ kamus-bahasa-rote/kamus.js
@@ -25,54 +25,6 @@ document.getElementById('pilih-bahasa').addEventListener('change', function() {
     bahasa2.innerHTML = "Rote Delha";
   }
 });
-
-
-// function search() {
-//   var terjemahan = document.getElementById("terjemahan");
-//   terjemahan.hidden = true;
-//   var input = document.getElementById("search").value.toLowerCase();
-//   var getLang = localStorage.getItem("bahasa");
-//   // Load the database file
-//   var xhr = new XMLHttpRequest();
-//   xhr.open("GET", "./kamus-bahasa-rote/kamus.txt", false);
-//   xhr.send(null);
-
-//   var lines = xhr.responseText.split("\n");
-
-//   // Search for the input keyword in the database
-//   var result = "";
-//   for (var i = 0; i < lines.length; i++) {
-//     var line = lines[i].split(",");
-//     var keyword = line[0].toLowerCase();
-//     var meaning = line[1];
-
-//     if(getLang === "1"){
-//       if (keyword === input) {
-//         result = meaning;
-//         break;
-//       } else if (input === "") {
-//         result = "Tidak diketahui";
-//       } else {
-//         result = "Tidak diketahui";
-       
-//       }
-//     }else if(getLang === "2"){
-//       if (input === meaning) {
-//         result = keyword;
-//         break;
-//       } else if (input == "") {
-//         result = "Tidak diketahui";
-//       } else {
-//         result = "Tidak diketahui";
-//       }
-//     }
-//   }
-
-//   // Display the search result
-//   var output = document.getElementById("result");
-//   output.innerHTML = result;
-// }
-
 function search() {
   terjemahan.hidden = true;
   var input = document.getElementById("search").value.toLowerCase();
